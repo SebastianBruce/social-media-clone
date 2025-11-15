50f06366a123c8c09f8fae5937e4c10bcf111075
SebastianBruce <sebastianbruceschool@gmail.com>
2025-11-15 13:51:41 -0500

    implemented follow/unfollow system and pages to view a users followers/following

--- lib/features/profile/presentation/pages/profile_page.dart
+++ lib/features/profile/presentation/pages/profile_page.dart
@@ -7,9 +7,12 @@ import 'package:social_media_clone/features/post/components/post_tile.dart';
 import 'package:social_media_clone/features/post/presentation/cubits/post_cubit.dart';
 import 'package:social_media_clone/features/post/presentation/cubits/post_states.dart';
 import 'package:social_media_clone/features/profile/presentation/components/bio_box.dart';
+import 'package:social_media_clone/features/profile/presentation/components/follow_button.dart';
+import 'package:social_media_clone/features/profile/presentation/components/profile_stats.dart';
 import 'package:social_media_clone/features/profile/presentation/cubits/profile_cubit.dart';
 import 'package:social_media_clone/features/profile/presentation/cubits/profile_states.dart';
 import 'package:social_media_clone/features/profile/presentation/pages/edit_profile_page.dart';
+import 'package:social_media_clone/features/profile/presentation/pages/follower_page.dart';
 
 class ProfilePage extends StatefulWidget {
   final String uid;
@@ -40,9 +43,55 @@ class _ProfilePageState extends State<ProfilePage> {
     profileCubit.fetchUserProfile(widget.uid);
   }
 
+  /*
+
+  FOLLOW / UNFOLLOW
+
+  */
+
+  void followButtonPressed() {
+    final profileState = profileCubit.state;
+    if (profileState is! ProfileLoaded) {
+      return; // return if profile is not loaded
+    }
+
+    final profileUser = profileState.profileUser;
+    final isFollowing = profileUser.followers.contains(currentUser!.uid);
+
+    // optimistically update UI
+    setState(() {
+      // unfollow
+      if (isFollowing) {
+        profileUser.followers.remove(currentUser!.uid);
+      }
+      // follow
+      else {
+        profileUser.followers.add(currentUser!.uid);
+      }
+    });
+
+    // perform actual toggle in cubit
+    profileCubit.toggleFollow(currentUser!.uid, widget.uid).catchError((error) {
+      // revert update if there's an error
+      setState(() {
+        // unfollow
+        if (isFollowing) {
+          profileUser.followers.add(currentUser!.uid);
+        }
+        // follow
+        else {
+          profileUser.followers.remove(currentUser!.uid);
+        }
+      });
+    });
+  }
+
   // BUILD UI
   @override
   Widget build(BuildContext context) {
+    // is own post
+    bool isOwnPost = (widget.uid == currentUser!.uid);
+
     // SCAFFOLD
     return BlocBuilder<ProfileCubit, ProfileState>(
       builder: (context, state) {
@@ -58,15 +107,16 @@ class _ProfilePageState extends State<ProfilePage> {
               foregroundColor: Theme.of(context).colorScheme.primary,
               actions: [
                 // edit profile button
-                IconButton(
-                  onPressed: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => EditProfilePage(user: user),
+                if (isOwnPost)
+                  IconButton(
+                    onPressed: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => EditProfilePage(user: user),
+                      ),
                     ),
+                    icon: const Icon(Icons.settings),
                   ),
-                  icon: const Icon(Icons.settings),
-                ),
               ],
             ),
 
@@ -112,6 +162,33 @@ class _ProfilePageState extends State<ProfilePage> {
                     ),
                   ),
                 ),
+
+                const SizedBox(height: 25),
+
+                // profile stats
+                ProfileStats(
+                  postCount: postCount,
+                  followerCount: user.followers.length,
+                  followingCount: user.following.length,
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => FollowerPage(
+                        followers: user.followers,
+                        following: user.following,
+                      ),
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 25),
+
+                // follow button
+                if (!isOwnPost)
+                  FollowButton(
+                    onPressed: followButtonPressed,
+                    isFollowing: user.followers.contains(currentUser!.uid),
+                  ),
 
                 const SizedBox(height: 25),
 
